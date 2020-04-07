c9f43dc6d7939a67864c7c69a3fe3d1420dee64c
Bertolda <boris.sen4ikov1994@yandex.ru>
2020-04-07 16:22:33 +0200

    create two activity

--- app/src/main/java/com/example/multitool/MainActivity.java
+++ app/src/main/java/com/example/multitool/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.multitool;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button appHider;
+    private Button addBlocker;
+    private Button smsReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initListener();
+    }
+    private void initListener(){
+        appHider = findViewById(R.id.button_app_hider);
+        addBlocker = findViewById(R.id.button_add_blocker);
+        smsReceiver = findViewById(R.id.button_sms_receiver);
+    }
+
+    public void enterPinCode(View view) {
+        Intent intent = new Intent(MainActivity.this, EnterPinCode.class);
+        startActivity(intent);
     }
 }
