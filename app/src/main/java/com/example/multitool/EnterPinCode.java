60a1460f9363aaeb268c0e38489c858fae66cfa4
Bertolda <boris.sen4ikov1994@yandex.ru>
2020-04-08 14:08:33 +0200

    add styles,mip map,string items

--- app/src/main/java/com/example/multitool/EnterPinCode.java
+++ app/src/main/java/com/example/multitool/EnterPinCode.java
@@ -2,13 +2,27 @@ package com.example.multitool;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class EnterPinCode extends AppCompatActivity {
+    private Button enter;
+    private Button cancel;
+    private EditText pinCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_pin_code);
+        enter = findViewById(R.id.button_enter);
+        cancel = findViewById(R.id.button_cancel);
+        pinCode = findViewById(R.id.edit_text_pin_code);
+    }
+
+
+    public void hideApp(View view) {
     }
 }
